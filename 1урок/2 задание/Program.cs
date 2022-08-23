c0340070c5488a4b1180b831dfd520a292b6d033
Димон <m-dimonn3@yandex.ru>
2022-08-23 22:15:27 +0300

    задание 2

---
+++ 1урок/2 задание/Program.cs
@@ -0,0 +1,31 @@
+﻿int a = 5;
+int b = 7;
+int c = 2;
+int d = 10;
+int f = -9;
+int t = -3;
+
+if (a > b)
+{
+    Console.WriteLine("Число " + a + " больше числа " + b);
+}
+else
+{
+    Console.WriteLine("Число " + b + " больше числа " + a);
+}
+if (c > d)
+{
+    Console.WriteLine("Число " + c + " больше числа " + d);
+}
+else
+{
+    Console.WriteLine("Число " + d + " больше числа " + c);
+}
+if (f > t)
+{
+    Console.WriteLine("Число " + f + " больше числа " + t);
+}
+else
+{
+    Console.WriteLine("Число " + t + " больше числа " + f);
+}
