397b6ae7913294b5dbc9657189f0ac638111a459
Димон <m-dimonn3@yandex.ru>
2022-08-23 23:59:59 +0300

    задание 4

--- 1урок/4 задание/Program.cs
+++ 1урок/4 задание/Program.cs
@@ -32,4 +32,70 @@ else
 
 }
 
+int a2 = 44;
+int b2 = 5;
+int c2 = 78;
+int Max2 = a2;
 
+if (Max2 > b2)
+{
+   if (Max2 > c2)
+    {
+        Console.WriteLine("число " + Max2 + " максимальное ");
+
+    }
+    else
+    {
+        Max2 = c2;
+        Console.WriteLine("число " + Max2 + " максимальное ");
+    }
+
+}
+else
+{
+    Max2=b2;
+    if (Max2>c2)
+    {
+        Console.WriteLine("число " + Max2 + " максимальное ");
+    }
+    else
+    {
+        Max2=c2;
+        Console.WriteLine("число " + Max2 + " максимальное ");
+    }
+
+}
+
+int a3 = 22;
+int b3 = 3;
+int c3 = 9;
+int Max3 = a3;
+
+if (Max3 > b3)
+{
+   if (Max3 > c3)
+    {
+        Console.WriteLine("число " + Max3 + " максимальное ");
+
+    }
+    else
+    {
+        Max3 = c3;
+        Console.WriteLine("число " + Max3 + " максимальное ");
+    }
+
+}
+else
+{
+    Max3=b3;
+    if (Max3>c3)
+    {
+        Console.WriteLine("число " + Max3 + " максимальное ");
+    }
+    else
+    {
+        Max3=c3;
+        Console.WriteLine("число " + Max3 + " максимальное ");
+    }
+
+}
