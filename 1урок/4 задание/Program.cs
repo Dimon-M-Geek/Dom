a0847ef0139524b9be0697321da02f8d37d81b03
Димон <m-dimonn3@yandex.ru>
2022-08-23 23:53:55 +0300

    задание 4

---
+++ 1урок/4 задание/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = 2;
+int b = 3;
+int c = 7;
+int Max = a;
+
+if (Max > b)
+{
+   if (Max > c)
+    {
+        Console.WriteLine("число " + Max + " максимальное ");
+
+    }
+    else
+    {
+        Max = c;
+        Console.WriteLine("число " + Max + " максимальное ");
+    }
+
+}
+else
+{
+    Max=b;
+    if (Max>c)
+    {
+        Console.WriteLine("число " + Max + " максимальное ");
+    }
+    else
+    {
+        Max=c;
+        Console.WriteLine("число " + Max + " максимальное ");
+    }
+
+}
+
+
