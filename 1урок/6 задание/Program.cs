ca708fac966e2025228c4e60c7e17482ee0d0420
Димон <m-dimonn3@yandex.ru>
2022-08-24 01:46:37 +0300

    задача 6

---
+++ 1урок/6 задание/Program.cs
@@ -0,0 +1,29 @@
+﻿int num = 4;
+if (num % 2 == 0)
+{
+    Console.WriteLine("число " + num + " четное");
+}
+else
+{
+    Console.WriteLine("число " + num + " нечетное");
+}
+
+int number = -3;
+if (number % 2 == 0)
+{
+    Console.WriteLine("число " + number + "четное");
+}
+else
+{
+    Console.WriteLine("число " + number + " нечетное");
+}
+
+int numb = 7;
+if (numb % 2 == 0)
+{
+    Console.WriteLine("число " + numb + "четное");
+}
+else
+{
+    Console.WriteLine("число " + numb + " нечетное");
+}
